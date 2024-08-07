e974a8e13bdcfdc41f000d5ab10a6d1be09a2921
smita <smitakpatil26@gmail.com>
2024-08-07 23:48:33 +0530

    create JWTAuthenticateion entry point

---
+++ JWTAuthenticationApplication/src/main/java/com/JWTAuthenticationApplication/security/JWTAUthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.JWTAuthenticationApplication.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class JWTAUthenticationEntryPoint implements AuthenticationEntryPoint{
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer=response.getWriter();
+        writer.print("Access Denied "+authException.getMessage());
+    }
+}
